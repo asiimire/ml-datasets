55483161263ceea7f7014ac9979561f885ce3af9
asiimire <101188715+asiimire@users.noreply.github.com>
2022-07-25 03:59:08 +0300

    Add files via upload

---
+++ ContactsManager.java
@@ -0,0 +1,8 @@
+public class ContactsManager {
+    public static void main(String [] args){
+        //Create the ContactsManager object
+        ContactsManager myContactsManager = new ContactsManager();
+        // Create a new Contacts object for Patricia
+        Contact frie
+    }
+}
